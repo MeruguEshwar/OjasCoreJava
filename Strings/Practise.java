8682a3bdea263c2c9d03be04301039f608ba318e
MeruguEshwar <merugueshwar93@gmail.com>
2022-06-28 20:49:23 +0530

    third commit

---
+++ Strings/Practise.java
@@ -0,0 +1,16 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Practise {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the string....");
+		String s1=scan.next();
+		int i=s1.length();
+		System.out.println(i);
+		
+	}
+
+}
